aa9faf83f9afc629be945a334f641b812c5b4ce4
stackOvercrow <gabriel-reis1999@hotmail.com>
2021-03-21 14:02:12 -0300

    Audio Playing

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'dart:io';
+import 'package:assets_audio_player/assets_audio_player.dart';
 import 'package:flutter/material.dart';
 
 class birthdayPerson {
@@ -23,7 +23,7 @@ void main() {
       'Muitos anos de vida, muita saúde e muita paz!';
   birthdayPerson.birthdayDate = DateTime.parse('2020-23-03');
   birthdayPerson.numberOfBirthdayPhotos = 10;
-  birthdayPerson.birthdayMusic = 'sound.mp3';
+  birthdayPerson.birthdayMusic = 'assets/sound.mp3';
   birthdayPerson.backgroundImage = 'assets/background.gif';
 
   runApp(MyApp());
@@ -33,22 +33,17 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    AssetsAudioPlayer.newPlayer().open(
+      Audio(birthdayPerson.birthdayMusic),
+      autoStart: true,
+      showNotification: false,
+      loopMode: LoopMode.single,
+    );
+
     return MaterialApp(
-      title: 'Happy Birthday',
+      title: 'Feliz Aniversário!',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primarySwatch: Colors.purple,
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
       home: MyHomePage(
@@ -60,14 +55,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   MyHomePage({Key key, this.title}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
   final String title;
 
   @override
@@ -94,16 +81,8 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     String birthdayPersonPhotos = 'assets/1.jpg';
 
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
       body: Container(
